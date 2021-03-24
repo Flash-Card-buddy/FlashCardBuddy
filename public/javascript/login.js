532985f4a794f0997439afd08e2240e67b5edd27
Dan Arbelo <govepitr@gmail.com>
2021-03-24 14:04:34 -0700

    Error persists for editing posts and viewing posts

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -18,6 +18,7 @@ async function signupFormHandler(event) {
     // check the response status
     if (response.ok) {
       console.log("::looks at camera:: I'm in..");
+    document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
     }

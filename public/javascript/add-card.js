0eb7c2cc63a208cfa4cf6f72c2ae396c9a2797af
bstanfield1992 <bstanfield1992@gmail.com>
2021-04-01 22:09:09 -0700

    styling for mobile cards

--- public/javascript/add-card.js
+++ public/javascript/add-card.js
@@ -44,7 +44,7 @@ async function finishDeckHandler(event) {
   const deck_id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
-  
+
   const response = await fetch(`/api/cards`, {
     method: 'POST',
     body: JSON.stringify({

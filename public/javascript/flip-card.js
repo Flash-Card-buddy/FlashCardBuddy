120fb68c56f729c89eede1728d873fb7fb4bfb74
Dan Arbelo <govepitr@gmail.com>
2021-04-02 14:00:54 -0700

    Fixed buttons&issue with flip not working on cards

--- public/javascript/flip-card.js
+++ public/javascript/flip-card.js
@@ -1,15 +1,15 @@
 // function flipCard(event) {
-var status = "front"
-document.querySelector(".flip-card").addEventListener("click", function () {
-if (status === "front") {
-    document.querySelector(".flip-card-inner").style="transform: rotateY(180deg)"
-    status = "back"
-  }
-  else {
-    document.querySelector(".flip-card-inner").style="transform: rotateY(360deg)"
-    status = "front"
-  }
-  console.log(status)
+// var status = "front"
+// document.querySelector(".flip-card").addEventListener("click", function () {
+// if (status === "front") {
+//     document.querySelector(".flip-card-inner").style="transform: rotateY(180deg)"
+//     status = "back"
+//   }
+//   else {
+//     document.querySelector(".flip-card-inner").style="transform: rotateY(360deg)"
+//     status = "front"
+//   }
+//   console.log(status)
 
-})
+// })
 // }

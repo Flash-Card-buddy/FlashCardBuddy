a23aaa5d54f597ad58cb42603544196f266eb60f
Dan Arbelo <govepitr@gmail.com>
2021-04-03 18:20:24 -0700

    No more comments.

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,33 +1,33 @@
-async function commentFormHandler(event) {
-  event.preventDefault();
+// async function commentFormHandler(event) {
+//   event.preventDefault();
 
-  const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+//   const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
 
-  const deck_id = window.location.toString().split('/')[
-    window.location.toString().split('/').length - 1
-  ];
+//   const deck_id = window.location.toString().split('/')[
+//     window.location.toString().split('/').length - 1
+//   ];
 
-  console.log(typeof deck_id)
-  console.log(deck_id)
+//   console.log(typeof deck_id)
+//   console.log(deck_id)
 
-  if (comment_text) {
-    const response = await fetch('/api/comments', {
-      method: 'POST',
-      body: JSON.stringify({
-        deck_id,
-        comment_text
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
+//   if (comment_text) {
+//     const response = await fetch('/api/comments', {
+//       method: 'POST',
+//       body: JSON.stringify({
+//         deck_id,
+//         comment_text
+//       }),
+//       headers: {
+//         'Content-Type': 'application/json'
+//       }
+//     });
   
-    if (response.ok) {
-      document.location.reload();
-    } else {
-      alert(response.statusText);
-    }
-  }
-}
+//     if (response.ok) {
+//       document.location.reload();
+//     } else {
+//       alert(response.statusText);
+//     }
+//   }
+// }
 
-document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
+// document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
